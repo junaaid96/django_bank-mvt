cb001a5df14b636b98cd326f21851903d6044e1b
junaaid96 <info.junaidulislam@gmail.com>
2024-01-31 11:31:33 +0600

    updates v1.8

---
+++ transactions/forms.py
@@ -0,0 +1,70 @@
+from django import forms
+from .models import Transaction
+
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['amount', 'transaction_type']
+
+    def __init__(self, *args, **kwargs):
+        # we will pass account object from view to form. so we need to pop it from kwargs.
+        self.account = kwargs.pop('account')
+        super().__init__(*args, **kwargs)
+        # we will handle this field in view so we disable it for user.
+        self.fields['transaction_type'].disabled = True
+        self.fields['transaction_type'].widget = forms.HiddenInput()
+
+    def save(self, commit=True):
+        # we are assigning account object to transaction instance.
+        self.instance.account = self.account
+        self.instance.balance_after_transaction = self.account.balance
+        return super().save()
+
+
+class DepositForm(TransactionForm):
+    def clean_amount(self):
+        amount = self.cleaned_data['amount']
+        min_deposit = 100
+        if amount < min_deposit:
+            raise forms.ValidationError(
+                f"Minimum deposit amount is ${min_deposit}")
+        return amount
+
+    # def save(self, commit=True):
+    #     self.instance.transaction_type = 'Deposit'
+    #     return super().save()
+
+
+class WithdrawForm(TransactionForm):
+    def clean_amount(self):
+        account = self.account
+        balance = account.balance
+        min_withdraw = 500
+        max_withdraw = 50000
+        amount = self.cleaned_dat.get('amount')
+        if amount < min_withdraw:
+            raise forms.ValidationError(
+                f"Minimum withdraw amount is ${min_withdraw}")
+        if amount > max_withdraw:
+            raise forms.ValidationError(
+                f"Maximum withdraw amount is ${max_withdraw}")
+        if amount > balance:
+            raise forms.ValidationError(
+                f"Insufficient balance. Your balance is ${balance}")
+        return amount
+
+
+class LoanRequestForm(TransactionForm):
+    def clean_amount(self):
+        account = self.account
+        balance = account.balance
+        min_loan = balance * 0.1
+        max_loan = balance * 0.5
+        amount = self.cleaned_data.get('amount')
+        if amount < min_loan:
+            raise forms.ValidationError(
+                f"You have to take minimum ${min_loan} loan")
+        if amount > max_loan:
+            raise forms.ValidationError(
+                f"You can take maximum ${max_loan} loan")
--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,107 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Transaction
+from .forms import DepositForm, WithdrawForm, LoanRequestForm
+from django.contrib import messages
+from django.http import HttpResponse
 
 # Create your views here.
+
+# we will inherit this view for all transaction such as deposit, withdrawal, transfer, loan request, etc.
+
+
+class CreateTransactionView(LoginRequiredMixin, CreateView):
+    template_name = ''
+    model = Transaction
+    title = ''
+    success_url = reverse_lazy('')
+
+    # passing the account object to the form. get_form_kwargs method is often used to pass additional parameters to a form when it is instantiated. This can be useful for dynamically customizing the behavior of a form based on the context of the view.
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        # kwargs['account'] = self.request.user.account
+        kwargs.update({
+            'account': self.request.user.account
+        })
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['title'] = self.title
+        context.update({
+            'title': self.title
+        })
+        return context
+
+
+class DepositMoney(CreateTransactionView):
+    form_class = DepositForm
+    title = 'Deposit Money'
+
+    def get_initial(self):
+        initial = {'transaction_type': 'Deposit'}
+        return initial
+
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        account = self.request.user.account
+        account.balance += amount
+        account.save(
+            update_fields=['balance']
+        )
+
+        messages.success(
+            self.request, f'You have successfully deposited ${amount}')
+
+        return super().form_valid(form)
+
+
+class WithdrawMoney(CreateTransactionView):
+    form_class = WithdrawForm
+    title = 'Withdraw Money'
+
+    def get_initial(self):
+        initial = {'transaction_type': 'Withdraw'}
+        return initial
+
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        account = self.request.user.account
+        account.balance -= amount
+        account.save(
+            update_fields=['balance']
+        )
+
+        messages.success(
+            self.request, f'You have successfully withdrawn ${amount}')
+
+        return super().form_valid(form)
+
+
+class LoanRequest(CreateTransactionView):
+    form_class = LoanRequestForm
+    title = 'Loan Request'
+
+    def get_initial(self):
+        initial = {'transaction_type': 'Loan'}
+        return initial
+
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        loan_request_count = Transaction.objects.filter(
+            account=self.request.user.account, transaction_type='Loan Request').count()
+        loan_approved_count = Transaction.objects.filter(
+            account=self.request.user.account, transaction_type='Loan').count()
+
+        if loan_request_count > 0:
+            return HttpResponse('You already have a loan request')
+        if loan_approved_count >= 3:
+            return HttpResponse('You have reached the maximum limit of loan request')
+
+        messages.success(
+            self.request, f'You have successfully requested ${amount} loan and awaiting for admin approval')
+
+        return super().form_valid(form)
